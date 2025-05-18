6276b1fc2d2a7e65440d213e7187879f32b22baf
cando12 <nantapornd56@gmail.com>
2025-05-18 16:15:18 +0700

    config supabase and tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
   import { supabase } from '@/lib/supabaseClient'
 
-  const { data, error } = await supabase.from('countries').select()
-
   console.log(supabase)
 </script>
 
 <template>
   <main>
-    <router-view />
+    <RouterView />
   </main>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import { h } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/project',
-      name: 'project',
-      component: () => import('@/views/ProjectView.vue')
-    },
-    {
-      path: '/project/:id',
-      name: 'project-single',
-      component: () => import('@/views/ProjectSingleView.vue')
-    },
-    {
-      path: '/:catchAll(.*)*',
-      name: 'not-found',
-      component: h(
-        'p',
-        { style: 'text-align: center; font-size: 2rem;' },
-        'Page not found'
-      )
-    }
-  ]
+  routes
 })
 
 export default router
