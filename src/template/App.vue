1aea7f1783feaf2ff7e848fc143be0abf65d4daf
cando12 <nantapornd56@gmail.com>
2025-05-18 21:36:52 +0700

    refactor router link to component

---
+++ src/template/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import TopNavbar from '@/components/Layout/TopNavbar.vue'
+  import SideBar from '@/components/Layout/SideBar.vue'
+</script>
+
+<template>
+  <SideBar />
+  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
+    <TopNavbar />
+    <main>
+      <RouterView />
+    </main>
+  </div>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,17 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), VueRouter(),tailwindcss()],
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        isCustomElement: (tag) => tag.startsWith('iconify-')
+      }
+    }
+  }), vueDevTools(), VueRouter(),tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+
 })
